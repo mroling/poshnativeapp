71f0d75827236ee19a9de1827533162a01acf96d
Dev <mroling>
2018-08-02 10:11:13 -0400

    commit 1

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import PoshApp from "./src/PoshApp.js";
+
+
+export default class App extends React.Component {
+  render() {
+    return  <PoshApp />;
+  }
+}
